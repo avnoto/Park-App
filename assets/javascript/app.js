3c918e0cc489d64720f1b9e7c39d463da42736d5
avnoto <alexandravnoto@gmail.com>
2020-05-25 03:57:32 -0700

    added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
   var infowindow;
 
+  //function to get Google map and embed in HTML
   function initMap() {
     var sacramento = new google.maps.LatLng(38.5618473, -121.5129521);
 
@@ -13,6 +14,7 @@ $(document).ready(function () {
       zoom: 11.5,
     });
 
+    //Used name parameter to show places only with 'park' in it's name
     var request = {
       location: sacramento,
       radius: '10000',
@@ -23,22 +25,25 @@ $(document).ready(function () {
     service.nearbySearch(request, callback); //im being charged for this so please do not for loop
   }
 
+  //callback for results of each place and markers for each
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
         createMarker(results[i]);
-        console.log(results[i]);
       }
     }
   }
 
+  //map is rendered on page load
   initMap();
 
+  //function to render markers for place located on map
   function createMarker(place) {
     var marker = new google.maps.Marker({
       map: map,
       position: place.geometry.location,
     });
+    //general information for parks when clicked on specific marker
     google.maps.event.addListener(marker, 'click', function () {
       $('.card-title').text(place.name);
       $('.openHours').text('Open: Sunrise to Sunset');
@@ -46,6 +51,7 @@ $(document).ready(function () {
       $('.ratingTotal').text(place.user_ratings_total + ' Reviews');
       $('.parkAddress').text(place.vicinity);
 
+      //star ratings, grabbing pngs and inserting in html
       if (place.rating < 2) {
         $('.star-container').html(
           $(`<img src="./assets/images/1star.png" width="25%" height="25%"/>`)
@@ -71,7 +77,7 @@ $(document).ready(function () {
           $(`<img src="./assets/images/5stars.png" width="30%" height="30%"/>`)
         );
       }
-
+      //features per park are hard-coded because it does not come back in the response for Places API
       if (place.name === 'William Land Regional Park') {
         $('.text-left').addClass('hide');
         $('.covid-text').addClass('hide');
@@ -154,6 +160,7 @@ $(document).ready(function () {
           'Until further notice, the spray feature at this park is closed due to the inability to distance 6 feet while using this feature.'
         );
         $('.covid-notice').removeClass('hide');
+        $('.parkAddress').text('310 Bowman Ave, Sacramento');
       }
       if (place.name === 'Mae Fong Park') {
         $('.text-left').addClass('hide');
